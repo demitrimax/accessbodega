8ba63caaf48a68f929403de5dfc9d12ea4f5d936
demitrimax <armandoaguilar1@yahoo.com>
2019-05-13 21:55:44 -0500

    modificaciones al sistema

---
+++ 201904/bodegajc.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localmaria
+ Source Server Type    : MariaDB
+ Source Server Version : 100314
+ Source Host           : localhost:3306
+ Source Schema         : bodegajc
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100314
+ File Encoding         : 65001
+
+ Date: 13/05/2019 13:59:50
+*/
+
